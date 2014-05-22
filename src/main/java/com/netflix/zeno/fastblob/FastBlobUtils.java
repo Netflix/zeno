daecee2c04e6ff7ae0c144eabae015663371b1a6
timurua <timurua@netflix.com>
2014-05-22 13:39:29 -0700

    - Fixed order for creating the images bitset

--- src/main/java/com/netflix/zeno/fastblob/FastBlobUtils.java
+++ src/main/java/com/netflix/zeno/fastblob/FastBlobUtils.java
@@ -10,8 +10,8 @@ public class FastBlobUtils {
             throw new IllegalArgumentException("while packing boolean array in int, the array length should be less than 32");
         }
 
-        int n = 0, l = a.length;
-        for (int i = 0; i < l; ++i) {
+        int n = 0;
+        for (int i = (a.length - 1); i >= 0; --i) {
             n = (n << 1) | (a[i] ? 1 : 0);
         }
         return n;
