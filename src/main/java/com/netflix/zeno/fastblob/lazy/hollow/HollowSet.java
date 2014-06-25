f7cc26fb2a8294ceba2c4f168b6e5f2e40fce133
dkoszewnik <drew.koszewnik@gmail.com>
2014-06-24 18:17:45 -0700

    HollowSet needs to use Set iterator instead of List iterator.

--- src/main/java/com/netflix/zeno/fastblob/lazy/hollow/HollowSet.java
+++ src/main/java/com/netflix/zeno/fastblob/lazy/hollow/HollowSet.java
@@ -4,6 +4,6 @@ package com.netflix.zeno.fastblob.lazy.hollow;
 public class HollowSet extends HollowCollection {
 
     public HollowOrdinalIterator ordinalIterator() {
-        return new HollowListOrdinalIterator(data, position);
+        return new HollowSetOrdinalIterator(data, position);
     }
 }
