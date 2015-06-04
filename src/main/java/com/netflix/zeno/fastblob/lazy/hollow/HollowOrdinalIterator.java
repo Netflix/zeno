3c32d9287d49bd02067b2f22774cf5212b92d080
dkoszewnik <drew.koszewnik@gmail.com>
2015-06-04 12:17:32 -0700

    Removing experiments

--- src/main/java/com/netflix/zeno/fastblob/lazy/hollow/HollowOrdinalIterator.java
+++
@@ -1,9 +0,0 @@
-package com.netflix.zeno.fastblob.lazy.hollow;
-
-public interface HollowOrdinalIterator {
-
-    public static final int NO_MORE_ORDINALS = Integer.MAX_VALUE;
-
-    public int next();
-
-}
--- src/main/java/com/netflix/zeno/fastblob/lazy/hollow/HollowSet.java
+++
@@ -1,9 +0,0 @@
-package com.netflix.zeno.fastblob.lazy.hollow;
-
-
-public class HollowSet extends HollowCollection {
-
-    public HollowOrdinalIterator ordinalIterator() {
-        return new HollowSetOrdinalIterator(data, position);
-    }
-}
